06ab42c0c07943d28185b1834edca8f270015c6d
= <=>
2024-11-29 11:37:23 +0100

    postgres DB containerized

--- scripts/db_conn.py
+++ scripts/db_conn.py
@@ -8,8 +8,17 @@ import os
 # Load environment variables from .env
 load_dotenv()
 
+
+# Retrieve the environment variables
+DATABASE_HOST = os.getenv('DATABASE_HOST', 'localhost')  # Default to localhost if not set
+DATABASE_PORT = os.getenv('DATABASE_PORT', '5432')
+DATABASE_USER = os.getenv('DATABASE_USER', 'postgres')
+DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD', 'admin')
+DATABASE_NAME = os.getenv('DATABASE_NAME', 'postgres')
+
+
 # Database URL (from .env)
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = db_url = f"postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
 
 # Create the engine
 engine = create_engine(DATABASE_URL)
